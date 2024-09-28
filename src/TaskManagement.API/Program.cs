915b58783fdd06b6a917f86874885674e4a350b0
Ivan Gyoshev <gyoshev.ivan@gmail.com>
2024-09-28 16:02:07 +0300

    Adds initial project structure

---
+++ src/TaskManagement.API/Program.cs
@@ -0,0 +1,18 @@
+namespace TaskManagement.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
